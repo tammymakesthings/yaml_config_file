5f053c8bac10a7bde1aa2ae61e14bf03037f760d
Tammy Cravit <tcravit@taylored-software.com>
2010-09-24 08:21:08 -0700

    Version bump to 0.1.6

--- lib/generators/yaml_config_file/templates/config_file.rb
+++ lib/generators/yaml_config_file/templates/config_file.rb
@@ -1,7 +1,7 @@
-# Initializer to load the YAML configuration file from config/<%= config_file_name %>
+# Initializer to load the YAML configuration file from config/<%= gen_file_name %>
 
 require 'yaml'
 require 'erb'
 
-config_file_yml =  YAML.load(ERB.new(File.read(File.join(Rails.root, "config", "<%= config_file_name %>.yml"))).result)
-<%= config_file_name.upcase %> = config_file_yml["global"].merge!(config_file_yml[Rails.env]).with_indifferent_access
+<%= gen_file_name %>_yml =  YAML.load(ERB.new(File.read(File.join(Rails.root, "config", "<%= gen_file_name %>.yml"))).result)
+<%= gen_file_name.upcase %> = <%= gen_file_name %>_yml["global"].merge!(<%= gen_file_name %>_yml[Rails.env]).with_indifferent_access
