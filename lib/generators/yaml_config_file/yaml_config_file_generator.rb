e3a89a2d22251b837d1990646529af603b959bc0
Tammy Cravit <tcravit@taylored-software.com>
2010-09-24 08:14:19 -0700

    Fixed arguments to generator

--- lib/generators/yaml_config_file/yaml_config_file_generator.rb
+++ lib/generators/yaml_config_file/yaml_config_file_generator.rb
@@ -2,14 +2,16 @@ class YamlConfigFileGenerator < Rails::Generators::NamedBase
   
   source_root File.expand_path('../templates', __FILE__)
   
-  argument :config_file_name, :type => :string, :default => "config_file" 
-  
   class_option :skip_yaml_file, :type => :boolean, :default => false, :description => "Skip generation of the YAML file"
   class_option :skip_initializer, :type => :boolean, :default => false, :description => "Skip generation of the initializer"  
   
   def yaml_config_file
-    template "config_file.yml", "config/#{config_file_name}.yml" unless options.skip_yaml_file?
-    template "config_file.rb", "config/initializers/#{config_file_name}.rb" unless options.skip_initializer?
+    template "config_file.yml", "config/#{file_name}.yml" unless options.skip_yaml_file?
+    template "config_file.rb", "config/initializers/#{file_name}.rb" unless options.skip_initializer?
   end
-    
+  
+  private
+  def file_name
+    file_name.underscore
+  end
 end
