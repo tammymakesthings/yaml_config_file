35a9226fa650f05b8cdefe814b9bb8b600d3d67b
Tammy Cravit <tcravit@taylored-software.com>
2010-10-11 12:46:28 -0700

    Fixed initializer to not error out if global or environment-specific config sections aren't found

--- lib/generators/yaml_config_file/templates/config_file.rb
+++ lib/generators/yaml_config_file/templates/config_file.rb
@@ -4,4 +4,4 @@ require 'yaml'
 require 'erb'
 
 <%= gen_file_name %>_yml =  YAML.load(ERB.new(File.read(File.join(Rails.root, "config", "<%= gen_file_name %>.yml"))).result)
-<%= gen_file_name.upcase %> = <%= gen_file_name %>_yml["global"].merge!(<%= gen_file_name %>_yml[Rails.env]).with_indifferent_access
+<%= gen_file_name.upcase %> = (<%= gen_file_name %>_yml["global"] || {}).merge!(<%= (gen_file_name %>_yml[Rails.env] || {})).with_indifferent_access
